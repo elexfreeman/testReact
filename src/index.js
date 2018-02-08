5d33ed10b48facb9424dae1dc4565c4dee4eaae6
UD <elextraza@gmail.com>
2018-02-09 01:24:15 +0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Test from './test';
+
+
+render(
+    <div>
+        <Test/>
+    </div>
+    ,document.getElementById('root')
+);
